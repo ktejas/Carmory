8b9c8bd9f5128f24441e4e31ae6888585f5aaee1
ktejas <krishnasalimeda@gmail.com>
2020-11-20 13:18:37 +0530

    Added Vehicle Controller, Collectible and Speed Boost

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc4125daf4482c630b19c9961ef1d538332d9cfb0dc4f5a433f33d536ebcc0e9
-size 34
+oid sha256:78477343ec2c489ce5a4faf616cf8addb5556f28c1e2869cf72bc2537aadae4f
+size 42
--- Source/VehicleGame/Private/Pawns/VehicleMovementComponentBoosted4w.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VehicleGame.h"
-#include "Pawns/VehicleMovementComponentBoosted4w.h"
-
-UVehicleMovementComponentBoosted4w::UVehicleMovementComponentBoosted4w(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
-{
-}
-
